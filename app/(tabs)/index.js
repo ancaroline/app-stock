21f557d14a605217d7d8b18ee838faad72031476
Anne Caroline <carolaragao94@gmail.com>
2024-07-25 10:26:05 -0300

    cover

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,23 +1,19 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, ImageBackground } from 'react-native'
 
 export default function App(){
+  const localImage = require("../../src/assets/cover.png")
   return(
-    <View style={styles.container}>
-      
-      <Text style={styles.title}>FIERCE TATTOARIA</Text>
-      
-      <Image
-      source={require("../../src/assets/fierce.png")}
-      style={styles.logo}
-      />
+      <ImageBackground source={localImage} style={styles.container}>
 
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Verificar Estoque</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Atualizar Estoque</Text>
-      </TouchableOpacity>
-    </View>
+        <Text style={styles.title}>FIERCE TATTOARIA</Text>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Verificar Estoque</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Atualizar Estoque</Text>
+        </TouchableOpacity>
+      </ImageBackground>
   )
 }
 
@@ -28,23 +24,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
-  logo:{
-    marginBottom: 20,
-  },
   title:{
     color: "white",
-    fontSize: 30,
+    fontSize: 35,
     marginBottom: 15,
+    marginTop: 50,
     fontWeight: 'bold',
-
+    fontFamily: 'Roboto',
+    backgroundColor: '#00000099',
+    padding: 16,
   },
   button:{
     backgroundColor: "white",
-    width: "80%",
+    width: "60%",
     height: 50,
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 15,
     borderRadius: 8,
+  },
+  buttonText:{
+    fontFamily: 'Roboto',
+    fontSize: 18,
+    fontWeight: 'bold',
   }
 })
